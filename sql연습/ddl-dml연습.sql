a0b3b41a008c3c6554f05ebeff80909ddce0ee7b
poscodx-bitacademy <poscodx.bitacademy@gmail.com>
2023-09-01 11:15:08 +0900

    transaction

--- sql연습/ddl-dml연습.sql
+++ sql연습/ddl-dml연습.sql
@@ -68,4 +68,18 @@ delete
  where no = 4;
 select * from member;
  
+select @@autocommit from dual;
 
+-- transaction begin
+set autocommit = 0;
+select @@autocommit from dual;
+
+insert
+  into member(email, name, dept, password)
+values ('kickscar4@gmail.com', '안대혁4', '개발팀4', password('1234'));
+
+select no, email, dept from member;
+
+commit;
+
+select no, email, dept from member;
